1cdf56d2a4dc3f981e6856dfa3f9c5be56514aff
GregoryTaran <34890989+GregoryTaran@users.noreply.github.com>
2025-10-20 16:05:11 +0100

    Update recorder-worklet.js

--- recorder-worklet.js
+++ recorder-worklet.js
@@ -2,7 +2,8 @@ class RecorderProcessor extends AudioWorkletProcessor {
   process(inputs) {
     const input = inputs[0];
     if (input && input[0]) {
-      this.port.postMessage(input[0]);
+      // ✅ создаём копию, иначе браузер отдаёт нули или мусор
+      this.port.postMessage(input[0].slice(0));
     }
     return true;
   }
