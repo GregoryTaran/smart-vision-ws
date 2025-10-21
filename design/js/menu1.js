fde8e9c0e8851f49ae38327157533e3312b11012
GregoryTaran <34890989+GregoryTaran@users.noreply.github.com>
2025-10-21 22:12:02 +0100

    Create menu1.js

---
+++ design/js/menu1.js
@@ -0,0 +1,12 @@
+export function renderMenu() {
+  return `
+    <h3>МЕНЮ 1</h3>
+    <ul>
+      <li><a href="#">Пункт меню 1</a></li>
+      <li><a href="#">Пункт меню 2</a></li>
+      <li><a href="#">Пункт меню 3</a></li>
+      <li><a href="#">Пункт меню 4</a></li>
+      <li><a href="#">Пункт меню 5</a></li>
+    </ul>
+  `;
+}
