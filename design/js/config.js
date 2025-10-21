d79dca8364395649b15b72a8da8e21ad47c15bd1
GregoryTaran <34890989+GregoryTaran@users.noreply.github.com>
2025-10-21 22:01:47 +0100

    Create config.js

---
+++ design/js/config.js
@@ -0,0 +1,13 @@
+// 🌐 Smart Vision config
+// Здесь храним все глобальные настройки проекта
+
+export const CONFIG = {
+  BASE_URL: "https://test.smartvision.life/",
+  API_URL: "https://test.smartvision.life/api/",
+  VERSION: "1.0.0",
+  PROJECT_NAME: "Smart Vision Design",
+};
+
+// Пример использования:
+// import { CONFIG } from "./config.js";
+// console.log(CONFIG.BASE_URL);
