6125ee70625dee567e71e9c80e2e3393552cf373
GregoryTaran <34890989+GregoryTaran@users.noreply.github.com>
2025-10-21 22:49:38 +0100

    Update menu1.js

--- design/js/menu1.js
+++ design/js/menu1.js
@@ -1,12 +1,14 @@
 export function renderMenu() {
   return `
-    <h3>МЕНЮ 1</h3>
-    <ul>
-      <li><a href="#">Пункт меню 1</a></li>
-      <li><a href="#">Пункт меню 2</a></li>
-      <li><a href="#">Пункт меню 3</a></li>
-      <li><a href="#">Пункт меню 4</a></li>
-      <li><a href="#">Пункт меню 5</a></li>
+    <div class="menu-header">
+      <span class="menu-title">МЕНЮ</span>
+      <button id="menu-close" class="menu-close">←</button>
+    </div>
+    <ul class="menu-list">
+      <li><a href="#">Главная страница</a></li>
+      <li><a href="#">Политика конфиденциальности</a></li>
+      <li><a href="#">Условия использования</a></li>
+      <li><a href="#">О сервисе</a></li>
     </ul>
   `;
 }
